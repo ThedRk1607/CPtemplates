273cb0b746ee5a158be9ee0ee8a0dada01149756
ThedRk1607 <128738915+ThedRk1607@users.noreply.github.com>
2023-05-24 22:37:43 +0530

    Create basicRecursion.cpp

---
+++ ImportantQue/basicRecursion.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i,a,n) for(int i = a; i < int(n);i++ )
+#define FORR(x,arr) for(auto& x:arr)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define ll              long long 
+#define db 				double
+#define pb              push_back
+#define all(v)             v.begin(),v.end()
+#define F               first
+#define S               second
+#define INF             1000000007
+#define popcount(x)     __builtin_popcountll(x)
+#define pll             pair<ll,ll>
+#define pii             pair<int,int>
+#define nl  			"\n"
+#define PI   			3.1415926536
+#define mp 				make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+
+using vi = vector<int>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+const int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1}; // for every grid problem!!
+ 
+template <typename A, typename B> A amax (A &a, B b){ if (b > a) a = b ; return a; }
+template <typename A, typename B> A amin (A &a, B b){ if (b < a) a = b ; return a; }
+
+vl subset;
+
+void Set(int k){
+	if(k==5){
+		FORR(x,subset)cout<<x+1<<" ";
+		cout<<nl;
+		return;
+	}
+	Set(k+1);
+	subset.pb(k);
+	Set(k+1);
+	subset.pop_back();
+}
+
+
+void solve(){
+	Set(0);
+	// for(int b=0;b<(1<<3);b++){
+	// 	vl sub;
+	// 	sub.clear();
+	// 	for(int i=0;i<3;i++){
+	// 		if(b&(1<<i))sub.pb(i);
+	// 	}
+	// 	FORR(x,sub)cout<<x<<" ";
+	// 	cout<<nl;
+
+	// }
+	
+
+   
+}
+	
+
+
+ 
+ 
+ 
+	
+ 
+int32_t main()
+{
+ 
+	#ifndef ONLINE_JUDGE
+ 
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	 #endif
+   
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+ 
+   int T=1;//cin >> T;
+   while(T--)solve();
+  
+	#ifdef DEBUG
+		cerr << "Runtine is: " << clock() * 1.0 / CLOCKS_PER_SEC << endl;
+	#endif
+    
+    return 0;
+    
+    }
