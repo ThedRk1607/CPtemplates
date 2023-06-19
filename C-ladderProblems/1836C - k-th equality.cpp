449142428ee8402568dd5d1d2fd4ac8045f78c70
ThedRk1607 <128738915+ThedRk1607@users.noreply.github.com>
2023-06-19 10:38:23 +0530

    Create 1836C - k-th equality.cpp

---
+++ C-ladderProblems/1836C - k-th equality.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i,a,n) for(int i = a; i < int(n);i++ )
+#define FORR(x,arr) for(auto& x:arr)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define ll               long long 
+#define ull              unsigned long long 
+#define db 				double
+#define pb              push_back
+#define all(v)             v.begin(),v.end()
+#define F               first
+#define S               second
+#define INF             1000000007
+#define popcount(x)     __builtin_popcountll(x)
+#define pll             pair<ll,ll>
+#define pii             pair<int,int>
+#define nl  			"\n"
+#define PI   			3.1415926536
+#define eps 0.00000001
+#define mpp 				make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define PQ priority_queue
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0x3f,sizeof(a))
+
+using vi = vector<int>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+const int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1}; // for every grid problem!!
+ 
+template <typename A, typename B> A amax (A &a, B b){ if (b > a) a = b ; return a; }
+template <typename A, typename B> A amin (A &a, B b){ if (b < a) a = b ; return a; }
+
+int power(int a, int e) {
+    if (e == 0) return 1;
+    return e == 1 ? a : a * power(a, e-1);
+}
+
+void ans(int a,int b){
+	cout << a << " + " << b << " = " << a+b <<endl;
+
+}
+   
+void solve(){
+	int a,b,c;cin>>a>>b>>c;
+	ll k;cin>>k;
+	bool good=false;
+
+	for(int i=power(10,a-1);i<power(10,a);i++){
+		 int left = std::max(power(10, b-1), power(10, c-1) - i);
+            int right = std::min(power(10, b)-1, power(10, c) - 1 - i);
+		if(left>right)continue;
+		int have=right-left+1;
+		if(k<=have){
+			good=true;
+			ans(i,left+k-1);
+			break;
+		}
+		k-=have;
+	}
+
+	if (!good) std::cout << "-1" << std::endl;
+}
+
+
+ 
+	
+
+
+
+int32_t main()
+{
+ 
+	#ifndef ONLINE_JUDGE
+ 
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	 #endif
+   
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+ 
+   int T=1;cin >> T;
+   while(T--)solve();
+  
+	#ifdef DEBUG
+		cerr << "Runtine is: " << clock() * 1.0 / CLOCKS_PER_SEC << endl;
+	#endif
+    
+    return 0;
+    
+    }
