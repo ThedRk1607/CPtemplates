91643d0df1201a6b4efd5b694ffd6ef9854a5a36
ThedRk1607 <128738915+ThedRk1607@users.noreply.github.com>
2023-07-03 17:50:29 +0530

    Update KnightsMove.cpp

--- DP Problems/div 3 practice problems/KnightsMove.cpp
+++ DP Problems/div 3 practice problems/KnightsMove.cpp
@@ -14,3 +14,24 @@ ZERO(dp);
 
 	cout<<dp[n-1][m-1];
 }
+
+//
+void solve(){
+
+  ll n=8;
+  ll grid[n][n];
+  FOR(i,0,n)FOR(j,0,8)cin>>grid[i][j];
+  FOR(i,0,n)FOR(j,0,8)dp[i][j]=INF;
+
+dp[7][0]=0;
+
+
+
+  for(int i=7;i>=0;i--){
+  	for(int j=0;j<8;j++){
+  		if(i<n-1)dp[i][j]=min(dp[i][j],grid[i][j]+dp[i+1][j]);
+  		if(j)dp[i][j]=min(dp[i][j],grid[i][j]+dp[i][j-1]);
+  		if(i<n-1 and j)dp[i][j]=min(dp[i][j],grid[i][j]+dp[i+1][j-1]);
+  	}
+  }
+  cout<<dp[0][7];
