c3b14c1ac5cf50fff4833e2d4ae3935f7c083101
ThedRk1607 <128738915+ThedRk1607@users.noreply.github.com>
2023-07-11 18:08:13 +0530

    Create gamesdp.cpp

---
+++ DP Problems/dp forms/gamesdp.cpp
@@ -0,0 +1,93 @@
+
+#include <bits/stdc++.h>
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b)for(int i=a;i<b;i++)
+#define FORR(x,arr) for(auto& x:arr)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define ll               long long 
+#define ull              unsigned long long 
+#define db              double
+#define pb              push_back
+#define all(v)             v.begin(),v.end()
+#define F               first
+#define S               second
+#define INF             1000000007
+#define popcount(x)     __builtin_popcountll(x)
+#define pll             pair<ll,ll>
+#define pii             pair<int,int>
+#define nl              "\n"
+#define PI              3.1415926536
+#define eps 0.00000001
+#define mpp                 make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define PQ priority_queue //by default provide largest element;
+#define PQS priority_queue<int,vector<int>,greater<int>> 
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0x3f,sizeof(a))
+
+using vi = vector<int>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+using vs = vector<string>;
+const int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1}; // for every grid problem!!
+const int dxn[8]={1,0,-1,0,-1,1,-1,1},dyn[8]={0,1,0,-1,-1,1,1,-1};
+template <typename A, typename B> A amax (A &a, B b){ if (b > a) a = b ; return a; }
+template <typename A, typename B> A amin (A &a, B b){ if (b < a) a = b ; return a; }
+
+
+
+int dp[1001];
+
+//rod cutting problem;
+
+int rec(int x){
+	if(x==0)return 0;
+
+
+if(dp[x]!=-1)return dp[x];
+	int ans=0;
+	for(int m=0;(1<<m)<=x;m++){
+		if(rec(x-(1<<m))==0){ //if any state is losing then this win and
+								//if all state is won then this lose;
+			ans=1;
+			break;
+		}
+	}
+
+	return dp[x]= ans;
+	
+
+}
+
+void solve(){
+
+  
+  	memset(dp,-1,sizeof(dp));
+  	cout<<rec(12);
+}
+
+
+int32_t main()
+{
+ 
+	#ifndef ONLINE_JUDGE
+ 
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	 #endif
+   
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+
+   int T=1;//cin >> T;
+   while(T--)solve();
+  
+	#ifdef DEBUG
+		cerr << "Runtine is: " << clock() * 1.0 / CLOCKS_PER_SEC << endl;
+	#endif
+    
+    return 0;
+    
+    }
