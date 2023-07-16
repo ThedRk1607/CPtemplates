3fb76f4c9f980e18e0c52d5168d4723166a2236b
ThedRk1607 <128738915+ThedRk1607@users.noreply.github.com>
2023-07-16 18:33:48 +0530

    Create MaxProdSub.cpp

---
+++ ImportantQue/MaxProdSub.cpp
@@ -0,0 +1,17 @@
+ int maxProduct(vector<int>& nums) {
+       int mx=-1e5;
+       int prod=1;
+       for(int i=0;i<nums.size();i++){
+           prod*=nums[i];
+           mx=max(prod,mx);
+           if(prod==0)prod=1;
+       }
+
+       prod=1;
+       for(int i=nums.size()-1;i>=0;i--){
+           prod*=nums[i];
+          mx=max(prod,mx);
+           if(prod==0)prod=1;
+       }
+       return mx;
+    }
