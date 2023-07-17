00b6412f7478cc2b299e486bd8689c5d78b666ac
ThedRk1607 <128738915+ThedRk1607@users.noreply.github.com>
2023-07-17 17:22:30 +0530

    Create Frog2.cpp

---
+++ DP Problems/AtcoderDpContest/Frog2.cpp
@@ -0,0 +1,85 @@
+
+#include <bits/stdc++.h>
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b)for(int i=a;i<b;i++)
+#define FORR(x,arr) for(auto& x:arr)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define ll               long long 
+#define ull              unsigned long long 
+#define db              double
+#define pb              push_back
+#define all(v)             v.begin(),v.end()
+#define F               first
+#define S               second
+#define INF             1000000007
+#define popcount(x)     __builtin_popcountll(x)
+#define pll             pair<ll,ll>
+#define pii             pair<int,int>
+#define nl              "\n"
+#define PI              3.1415926536
+#define eps 0.00000001
+#define mpp                 make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define PQ priority_queue //by default provide largest element;
+#define PQS priority_queue<ll,vector<ll>,greater<ll>> 
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0x3f,sizeof(a))
+
+using vi = vector<int>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+using vs = vector<string>;
+const int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1}; // for every grid problem!!
+const int dxn[8]={1,0,-1,0,-1,1,-1,1},dyn[8]={0,1,0,-1,-1,1,1,-1};
+template <typename A, typename B> A amax (A &a, B b){ if (b > a) a = b ; return a; }
+template <typename A, typename B> A amin (A &a, B b){ if (b < a) a = b ; return a; }
+
+const int N=1e5+5;
+ll dp[N];
+
+void solve(){
+	int n,k;cin>>n>>k;
+	vi h(n);FOR(i,1,n+1)cin>>h[i];
+	for(int i=0;i<=n+1;i++)dp[i]=1e18;
+
+	
+	dp[1]=0;
+	for(int i=2;i<=n;i++){
+		for(int j=1;j<=k;j++){
+			
+			if(i-j>0)dp[i]=min(dp[i],abs(h[i]-h[i-j])+dp[i-j]);
+
+			
+		}
+	}
+	cout<<dp[n];
+
+}
+	
+
+
+
+int32_t main()
+{
+ 
+	#ifndef ONLINE_JUDGE
+ 
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	 #endif
+   
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+
+   int T=1;//cin >> T;
+   while(T--)solve();
+  
+	#ifdef DEBUG
+		cerr << "Runtine is: " << clock() * 1.0 / CLOCKS_PER_SEC << endl;
+	#endif
+    
+    return 0;
+    
+    }
