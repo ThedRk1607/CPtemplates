cac71c39879c9ad761b1bbc97b8cf665932d05bd
ThedRk1607 <128738915+ThedRk1607@users.noreply.github.com>
2023-07-18 17:54:16 +0530

    Create Coin.cpp

---
+++ DP Problems/AtcoderDpContest/Coin.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b)for(int i=a;i<b;i++)
+#define FORR(x,arr) for(auto& x:arr)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define ll               long long 
+#define ull              unsigned long long 
+#define db              double
+#define pb              push_back
+#define all(v)             v.begin(),v.end()
+#define F               first
+#define S               second
+#define INF             1000000007
+#define popcount(x)     __builtin_popcountll(x)
+#define pll             pair<ll,ll>
+#define pii             pair<int,int>
+#define nl              "\n"
+#define PI              3.1415926536
+#define eps 0.00000001
+#define mpp                 make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define PQ priority_queue //by default provide largest element;
+#define PQS priority_queue<ll,vector<ll>,greater<ll>> 
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0x3f,sizeof(a))
+
+using vi = vector<int>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+using vs = vector<string>;
+const int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1}; // for every grid problem!!
+const int dxn[8]={1,0,-1,0,-1,1,-1,1},dyn[8]={0,1,0,-1,-1,1,1,-1};
+template <typename A, typename B> A amax (A &a, B b){ if (b > a) a = b ; return a; }
+template <typename A, typename B> A amin (A &a, B b){ if (b < a) a = b ; return a; }
+
+const int mod=(int)1e9+7;
+double dp[3005][3005];
+
+void solve(){
+	int n;cin>>n;
+	vector<double>p(n);
+	FORR(x,p)cin>>x;
+
+
+dp[0][0]=(1-p[0]);
+dp[0][1]=p[0];
+
+	for(int i=1;i<n;i++){
+		for(int j=0;j<3005;j++){
+			//tails
+			dp[i][j]+=(1-p[i])*dp[i-1][j];
+			//head
+			if(j)dp[i][j]+=(p[i])*dp[i-1][j-1];
+		}
+	}
+	double ans=0;
+	for(int i=n/2+1;i<3000;i++)ans+=dp[n-1][i];
+		cout<<fixed<<setprecision(10);
+	cout<<ans<<nl;
+	
+}
+
+
+int32_t main()
+{
+ 
+	#ifndef ONLINE_JUDGE
+ 
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	 #endif
+   
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+
+   int T=1;//cin >> T;
+   while(T--)solve();
+  
+	#ifdef DEBUG
+		cerr << "Runtine is: " << clock() * 1.0 / CLOCKS_PER_SEC << endl;
+	#endif
+    
+    return 0;
+    
+    }
