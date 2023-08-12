27bc87797b7c9afc2e857dd26b512e4def87a5c5
ThedRk1607 <128738915+ThedRk1607@users.noreply.github.com>
2023-08-13 00:19:40 +0530

    Create startingPtLoop.cpp

---
+++ InterviewPrep/linkedList/startingPtLoop.cpp
@@ -0,0 +1,21 @@
+node* detectCycle(node* head) {
+    if(head == NULL||head->next == NULL) return NULL;
+        
+    node* fast = head;
+    node* slow = head;
+    node* entry = head;
+        
+    while(fast->next != NULL&&fast->next->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+            
+        if(slow == fast) {
+            while(slow != entry) {
+                slow = slow->next;
+                entry = entry->next;
+            }
+            return slow;
+        }
+    }
+    return NULL;
+}
