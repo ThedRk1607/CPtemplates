1cf93ec668339bcd09baaedc963ce8e9701deef3
ThedRk1607 <128738915+ThedRk1607@users.noreply.github.com>
2023-09-10 13:28:07 +0530

    Update rangeBitwiseAnd.cpp

--- Notes/BitsMask/rangeBitwiseAnd.cpp
+++ Notes/BitsMask/rangeBitwiseAnd.cpp
@@ -13,3 +13,15 @@ int rangeBitwiseAnd(int m, int n) {
             }
              return (m<<s);
         }
+
+int rangeBitwiseAnd(int m,int n){
+	int ans=0;
+	for(int bit=30;bit>=0;bit--){
+		if((m&(1<<bit))!=(n&(1<<bit)))break;
+		else {
+			ans|=(m&(1<<bit));
+		}
+	}
+	return ans;
+}
+
