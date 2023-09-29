f80b296d4066bd9532999c2061c8ba06fd549151
ThedRk1607 <128738915+ThedRk1607@users.noreply.github.com>
2023-09-30 00:51:43 +0530

    Update Sieve.cpp

--- MathsForCp/Sieve.cpp
+++ MathsForCp/Sieve.cpp
@@ -1,11 +1,18 @@
+const int N=1000;
+vector<bool> is_prime(N, true);
+vector<int>lp(N),hp(N);
 
-int n;
-vector<bool> is_prime(n+1, true),primes;
+void sieve(){
 is_prime[0] = is_prime[1] = false;
-for (int i = 2; i * i <= n; i++) {
+for (int i = 2; i <N; i++) {
     if (is_prime[i]) {
-      primes.pb(i);
-        for (int j = i * i; j <= n; j += i)
-            is_prime[j] = false;
+    	lp[i]=hp[i]=i;
+        for (int j = 2 * i; j <N; j += i)
+           {
+           	 is_prime[j] = false;
+           	 hp[j]=i;
+           	 if(lp[j]==0)lp[j]=i;
+           }
     }
+}
 }
