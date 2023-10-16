88edf7d95333641b832f82918f68f82a40d6020f
ThedRk1607 <128738915+ThedRk1607@users.noreply.github.com>
2023-10-16 23:49:32 +0530

    Create 407B - Long Path.cpp

---
+++ DP Problems/407B - Long Path.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b)for(int i=a;i<b;i++)
+#define FORR(x,arr) for(auto& x:arr)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define ll               long long 
+#define ull              unsigned long long 
+#define db              double
+#define pb              push_back
+#define all(v)             v.begin(),v.end()
+#define F               first
+#define S               second
+#define INF             1000000007
+#define popcount(x)     __builtin_popcountll(x)
+#define pll             pair<ll,ll>
+#define pii             pair<int,int>
+#define nl              "\n"
+#define PI              3.1415926536
+#define eps 0.00000001
+#define mpp                 make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define PQ priority_queue<pii> //by default provide largest element;
+#define RS set <ll, greater <ll> >
+#define PQS priority_queue<pll,vector<pll>,greater<pll>> 
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0x3f,sizeof(a))
+
+using vi = vector<int>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+using vs = vector<string>;
+const int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1}; // for every grid problem!!
+const int dxn[8]={1,0,-1,0,-1,1,-1,1},dyn[8]={0,1,0,-1,-1,1,1,-1};
+template <typename A, typename B> A amax (A &a, B b){ if (b > a) a = b ; return a; }
+template <typename A, typename B> A amin (A &a, B b){ if (b < a) a = b ; return a; }
+
+const int N=1e3+5,mod=1e9+7;
+
+vector<ll>dp(N,1);
+
+
+void solve(){
+	int n;cin>>n;
+	vl p(n+1);FOR(i,1,n+1)cin>>p[i];
+	dp[0]=0;
+	for(int i=1;i<=n;i++){
+		dp[i]=(2*dp[i-1]%mod+2-dp[p[i]-1]+mod)%mod;
+	}
+	
+	
+cout<<dp[n];
+
+}
+
+
+int32_t main()
+{
+ 
+	#ifndef ONLINE_JUDGE
+ 
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	 #endif
+   
+   ios_base::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+
+    
+   
+	
+
+   int T=1;//cin >> T;
+   while(T--)solve();
+  
+	#ifdef DEBUG
+		cerr << "Runtine is: " << clock() * 1.0 / CLOCKS_PER_SEC << endl;
+	#endif
+    
+    return 0;
+    
+    }
