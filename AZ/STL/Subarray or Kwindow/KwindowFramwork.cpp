359ec712d5c086b93b8747db189f1c330616af76
ThedRk1607 <128738915+ThedRk1607@users.noreply.github.com>
2024-04-09 08:54:52 +0530

    Update KwindowFramwork.cpp

--- AZ/STL/Subarray or Kwindow/KwindowFramwork.cpp
+++ AZ/STL/Subarray or Kwindow/KwindowFramwork.cpp
@@ -11,3 +11,29 @@
  	} 
   }
 
+//using monotone deque;
+struct monotone_deque{
+	deque<int>dq;
+	void insert(int x){
+		while(!dq.empty() and dq.back()>x)dq.pop_back();
+		dq.pb(x);
+	}
+
+	void erase(int x){
+		if(dq.front()==x)dq.pop_front();
+	}
+	int getmin(){
+		return dq.front();
+	}
+};
+
+void solve(){
+ 	int n,k;cin>>n>>k;
+ 	vi v(n);repp(x,v)cin>>x;
+ 	monotone+deque ms;
+ 	rep(i,0,n){
+ 		ms.insert(v[i]);
+ 		if(i-k>=0)ms.erase(ms.find(v[i]));
+ 		if(ms.size()==k)cout<<(*ms.begin())<<nl;
+ 	} 
+  }
