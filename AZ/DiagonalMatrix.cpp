05528c98b88e0ed49a1f68efcd333172e29eed13
Deepak kumar <128738915+ThedRk1607@users.noreply.github.com>
2025-01-02 10:23:15 +0530

    Update DiagonalMatrix.cpp

--- AZ/DiagonalMatrix.cpp
+++ AZ/DiagonalMatrix.cpp
@@ -8,3 +8,13 @@ vector<vector<int>>v(n,vector<int>(n));
 			ans[d]=max(ans[d],-v[i][j]);//max of all diagonals elements;
 		}
 	}
+
+//digonal for element vise;
+ for(int i=1;i<=n;i++){
+  	int x1=i,y1=0,x2=0,y2=n-i;
+  	int ones=0;
+  	for(int j=0;j<n and x1+j<n and y1+j<n;j++)ones+=v[x1+j][y1+j]-'0';
+  	for(int j=0;j<n and x2+j<n and y2+j<n;j++)ones+=v[x2+j][y2+j]-'0';
+  		amax(mx,ones);
+
+  }
