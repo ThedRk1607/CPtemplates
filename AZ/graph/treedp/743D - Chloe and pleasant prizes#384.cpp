087d1bf1c40764b8edc0668cf8fe864423ef45b5
Deepak kumar <128738915+ThedRk1607@users.noreply.github.com>
2025-04-18 12:20:35 +0530

    Create 743D - Chloe and pleasant prizes#384.cpp

---
+++ AZ/graph/treedp/743D - Chloe and pleasant prizes#384.cpp
@@ -0,0 +1,47 @@
+int n;int ans;
+vector<int>g[N],subt(N),a(N),mxsub(N,-inf);
+void dfs(int node,int p=-1){
+	 subt[node]=a[node];mxsub[node]=-inf;
+	 for(auto ch:g[node]){
+	 	if(ch==p)continue;
+	 	dfs(ch,node);
+	 	subt[node]+=subt[ch];
+	 	mxsub[node]=max(mxsub[node],mxsub[ch]);	
+	 }
+	mxsub[node]=max(mxsub[node],subt[node]);	
+ }
+
+ void dfs2(int node,int out,int p=-1){
+ 	 if(out!=-inf)ans=max(ans,subt[node]+out);
+ 	 vii mxtwo;
+ 	 for(auto ch:g[node]){
+ 	 	if(ch==p)continue;
+ 	 	mxtwo.pb({mxsub[ch],ch});sort(all(mxtwo),greater<pii>());
+ 	 	if(mxtwo.size()==3)mxtwo.pop_back();
+ 	 }
+ 	 mxtwo.pb({-inf,-1});
+ 	 for(auto ch:g[node]){
+ 	 	if(ch==p)continue;
+ 	 	int cur=(mxtwo[0].S==ch)?mxtwo[1].F:mxtwo[0].F;
+ 	 	dfs2(ch,max(out,cur),node);
+ 	}
+ }
+
+void solve(){		
+ 	cin>>n;
+ 	rep(i,0,n)g[i].clear();
+ 	rep(i,0,n)cin>>a[i];
+ 	rep(i,0,n-1){
+ 		int u,v;cin>>u>>v;u--,v--;
+ 		g[u].pb(v);g[v].pb(u);
+ 	}
+ ans=-inf;
+  dfs(0);
+  dfs2(0,-inf);
+  if(ans==-inf){cout<<"Impossible\n";return;}
+ cout<<ans;
+  
+	
+}
+
+
