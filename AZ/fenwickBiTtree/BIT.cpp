02bba872a661f8bc68b8a424e1c9a33dfe97d986
Deepak kumar <128738915+ThedRk1607@users.noreply.github.com>
2026-01-04 07:18:40 +0530

    Update BIT.cpp

--- AZ/fenwickBiTtree/BIT.cpp
+++ AZ/fenwickBiTtree/BIT.cpp
@@ -1,3 +1,59 @@
+
+
+int n,q,a[N];
+
+struct Fenwick{
+    int sum[N];
+    void inline add(int x,int k){
+        while(x<=500001){
+            sum[x]+=k;
+            x+=lowbit(x);
+        }
+    }
+    int inline ask(int x){
+        int res=0;
+        while(x){
+            res+=sum[x];
+            x^=lowbit(x);
+        }
+        return res;
+    }
+    int inline ask(int l,int r){
+        return ask(r)-ask(l-1);
+    }
+}t1,t2;
+
+
+void solve(){
+	int n,q;cin>>n>>q;
+	rep(i,1,n+1){
+		cin>>a[i];t1.add(a[i]+1,1);t2.add(a[i]+1,a[i]);
+	}
+	while(q--){
+		 int op;
+        cin >> op;
+        if(op==2){
+            int l,r;
+            cin >> l >> r;
+            if(l>r){
+                cout << l*n << endl;
+            }else{
+                cout << t1.ask(l)*l+t2.ask(l+1,r+1)+t1.ask(r+2,500001)*r << endl;
+            }
+        }else{
+            int x,y;
+            cin >> x >> y;
+            t1.add(a[x]+1,-1);
+            t2.add(a[x]+1,-a[x]);
+            t1.add(y+1,1);
+            t2.add(y+1,y);
+            a[x]=y;
+        }
+	}
+}
+
+
+
 template <class T> class BIT {
   private:
 	int size;
